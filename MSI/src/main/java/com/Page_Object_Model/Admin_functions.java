347d8a1cc6a1167ef487725765798c8add7b2c48
Poovarasan <poovarasan@mindwaveventures.com>
2024-09-25 22:42:01 +0530

    address to city

--- MSI/src/main/java/com/Page_Object_Model/Admin_functions.java
+++ MSI/src/main/java/com/Page_Object_Model/Admin_functions.java
@@ -16,7 +16,7 @@ public class Admin_functions extends Base_Class {
 	private WebElement admin_Fun;
 	
 	@FindBy(xpath = "//a[@class='\"+exDay+\"']")
-	private WebElement hbyuhbty;
+	private WebElement address;
 	
 
 	
